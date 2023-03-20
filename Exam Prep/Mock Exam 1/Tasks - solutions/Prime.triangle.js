7ee510465a051bff85906c1316531c1ae3af8ab3
Ilia Popov <125738297+ipp02@users.noreply.github.com>
2023-03-20 09:08:13 +0200

    Update Prime.triangle.js

--- Exam Prep/Mock Exam 1/Tasks - solutions/Prime.triangle.js
+++ Exam Prep/Mock Exam 1/Tasks - solutions/Prime.triangle.js
@@ -8,46 +8,33 @@ let gets =
       arr[index++]
   )(input, 0);
 
-let numbers = +gets();
+let n = Number(gets());
+let row = "";
 
-for (let i=0; i < numbers; i++) {
-    let currVar = i+1;
-    let str = '';
-
-    if(currVar < 4) {
-        for(let k=1; k <= currVar; k++) {
-            str += '1';
-        }
-        print(str)
-    } else if(currVar >= 4) {
-        str = '111';
-        let isPrime = true;
-
-        for(let j = 2; j <= Math.sqrt(currVar); j++) {
-            if(currVar % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if(isPrime) {
-            for(let j = 4; j <= currVar; j++) {
-                isPrime = true;
-                for(let k = 2; k <= Math.sqrt(j); k++) {
-                    if(j % k === 0) {
-                        isPrime = false;
-                        break;
-                        } else {
-                            continue;
-                        }
-                    }
-                    
-                    if(isPrime) {
-                        str += '1';
-                    } else {
-                        str += '0';
-                    }
-                }
-                print(str)
-            }
-        }
+for (let i = 1; i <= n; i++) {
+  let isPrime = true;
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
     }
+  }
+
+  if (isPrime) {
+    for (let j = row.length + 1; j <= i; j++) {
+      let localPrime = true;
+      for (let k = 2; k <= Math.sqrt(j); k++) {
+        if (j % k === 0) {
+          localPrime = false;
+          break;
+        }
+      }
+      if (localPrime) {
+        row += "1";
+      } else {
+        row += "0";
+      }
+    }
+    print(row);
+  }
+}
