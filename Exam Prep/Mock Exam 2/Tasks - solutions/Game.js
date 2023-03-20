22e326583151859acb8ec6e476c5df2f715c00d5
Ilia Popov <125738297+ipp02@users.noreply.github.com>
2023-03-20 14:46:29 +0200

    Update Game.js

--- Exam Prep/Mock Exam 2/Tasks - solutions/Game.js
+++ Exam Prep/Mock Exam 2/Tasks - solutions/Game.js
@@ -8,18 +8,7 @@ let gets =
       arr[index++]
   )(input, 0);
 
-let inputNum = gets();
-let n1 = +inputNum[0], n2 = +inputNum[1], n3 = +inputNum[2];
-let maxN = n1 * n2 * n3;
-for(let i =0; ; i++) {
-if (n1 * n2 + n3 > maxN) {
-  maxN = n1 * n2 + n3;
-} else if (n1 + n2 * n3 > maxN) {
-  maxN = n1 + n2 * n3;
-} else if (n1 + n2 + n3 > maxN) {
-  maxN = n1 + n2 + n3;
-} else {
-  break;
-}
-}
-print(maxN);
+let n = gets().split('').map(Number);
+
+let max = Math.max((n[0] * n[1] * n[2]), (n[0] * n[1] + n[2]), (n[0]+ n[1] * n[2]), (n[0] + n[1] + n[2]));
+print(max);
