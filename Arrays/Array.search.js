2500db5fa0847836210a0c6fd6968557fb29f143
Ilia Popov <125738297+ipp02@users.noreply.github.com>
2023-03-22 12:29:11 +0200

    Update Array.search.js

--- Arrays/Array.search.js
+++ Arrays/Array.search.js
@@ -8,25 +8,22 @@ let gets =
       arr[index++]
   )(input, 0);
 
-let list = gets().split(",").map(Number);
-let str = '';
+let list = gets().split(",").map(Number).sort((a, b) => a - b);
+let missing = [];
 
-for (let i = 0; i < list.length; i++) {
-    let counter = 0;
-  for (let j = 0; j < list.length; j++) {
-    if (i + 1 === list[j]) {
+for(let i=0; i < list.length; i++) {
+  let isFound = false;
+  for(let j=0; j < list.length; j++) {
+    if(i+1 === list[j]) {
+      isFound = true;
       break;
     } else {
-        counter++;
+      continue;
   }
 }
-  if (counter == list.length) {
-    if (str === '') {
-        str += (i+1);
-    } else {
-        str += ',' + (i+1);
-    }
+  if(!isFound) {
+    missing.push(i+1);
   }
 }
 
-print(str)
+print(missing);
